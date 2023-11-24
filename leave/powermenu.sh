f8e3486befaf1d2c5d2af49055a624b328f115ba
JeztC <dagreenrs@gmail.com>
2023-11-24 22:43:04 +0200

    feat: add power button bash script

---
+++ leave/powermenu.sh
@@ -0,0 +1,7 @@
+rofi \
+  -show p \
+  -modi p:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
+  -font "JetBrains Mono NF 16" \
+  -theme-str 'window {width: 11em;} listview {lines: 6;}' \
+  -location 3 \
+  -yoffset 30
