4f318009dde61d1f8523f2602352d2141b51580f
JeztC <dagreenrs@gmail.com>
2023-12-05 21:58:18 +0000

    feat: change the theme a bit for dracula theme

--- leave/powermenu.sh
+++ leave/powermenu.sh
@@ -1,7 +1,7 @@
 rofi \
   -show p \
   -modi p:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
-  -font "JetBrains Mono NF 16" \
+  -font "JetBrains Mono NF 24" \
   -theme-str 'window {width: 11em;} listview {lines: 6;}' \
   -location 3 \
   -yoffset 30
